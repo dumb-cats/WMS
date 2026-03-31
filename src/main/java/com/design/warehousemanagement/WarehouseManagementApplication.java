da5c121fc7ae683d464e7acd7a79ee49b94bc059
wanshang121 <3340440276@qq.com>
2026-03-31 16:00:26 +0800

    排除暂时不用的类

--- src/main/java/com/design/warehousemanagement/WarehouseManagementApplication.java
+++ src/main/java/com/design/warehousemanagement/WarehouseManagementApplication.java
@@ -3,7 +3,11 @@ package com.design.warehousemanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration.class
+        }
+)
 public class WarehouseManagementApplication {
 
     public static void main(String[] args) {
